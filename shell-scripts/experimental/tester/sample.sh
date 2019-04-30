fcfca407762468656e4da9c10c127105732fe752
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-04-30 21:54:14 +0900

    Add sample codes and comments

--- shell-scripts/experimental/tester/sample.sh
+++ shell-scripts/experimental/tester/sample.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# You define testcase with defining function
+# which name is test*
 test_add() {
     assert 2 -eq $(( 1 + 1 )) 
 }
@@ -12,6 +14,19 @@ test_maybe_failed_print_lineno() {
     # If you want to print the line number without bash extention,
     # please use `eval $assert' instead of `assert'.
     eval $assert 1 -eq $(( 1 + 1 )) 
+}
+
+# This is a wrong test definition.
+# Do not put a space between function name and ().
+# tester can't detect test case.
+test_wrong_definition () {
+    assert 1 -eq 2
+}
+
+# This is a wrong test definition.
+# test function must start from line head.
+    test_wrong_definition() {
+    assert 1 -eq 2
 }
 
 
