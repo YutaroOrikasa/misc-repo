a7bd64bbd9b0073590742cc0057ef76fb634eea4
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-05-03 20:33:19 +0900

    [tester.sh] Print message on command's error exitting

--- shell-scripts/experimental/tester/tester.sh
+++ shell-scripts/experimental/tester/tester.sh
@@ -7,20 +7,39 @@ export _TESTER_NOW_TESTING="$testee"
 
 assert() {
     test "$@" || {
-        printf '%-6s %s\n' Faild "$_TESTER_TESTING_FUNC"
+        printf '%-6s %s\n' Failed "$_TESTER_TESTING_FUNC"
         if [ -n "$BASH_VERSION" ];then
             _TESTER_ASSERTION_LINENO="${BASH_LINENO[0]}"
         fi
-        printf '    assertion faild: %s: line %s: assert %s\n' \
+        printf '    assertion failed: %s: line %s: assert %s\n' \
                "$_TESTER_NOW_TESTING" \
                "${_TESTER_ASSERTION_LINENO:-?}" \
                "$*"
+
+        # avoid executing _tester_onexit
+        trap - EXIT
+
         exit 1
     }
 }
 
 # use for `'eval $assert'
 assert='_TESTER_ASSERTION_LINENO=$LINENO assert'
+
+
+_tester_onexit() {
+    _tester_status=$?
+    test x"$_tester_status" = x"0" && return
+
+    printf '%-6s %s\n' Error "$_TESTER_TESTING_FUNC"
+
+    # We don't have surely method to get lineno where trap occured,
+    # so we print lineno as '?'.
+    printf '    command error exited: %s: line ?: status %s\n' \
+           "$_TESTER_NOW_TESTING" \
+           "$_tester_status"
+}
+
 
 test_failed=0
 
@@ -29,7 +48,10 @@ for test_func in $(grep '^test[^ ]*()' <"$testee" \
                          | awk '{ print $1 }')
 do
     export _TESTER_TESTING_FUNC="$test_func"
-    ("$test_func")
+    (
+        trap '_tester_onexit' EXIT
+        "$test_func"
+    )
     if [ $? = 0 ];then
         printf '%-6s %s\n' OK "$test_func"
     else
