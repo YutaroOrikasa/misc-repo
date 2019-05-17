bd21c5ca670420f8ff4b9036e9b4cef6ec3436ac
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-05-18 02:43:03 +0900

    [executable-archive] Simplify codes for self script path detection

--- shell-scripts/executable-archive/header.sh
+++ shell-scripts/executable-archive/header.sh
@@ -5,25 +5,11 @@ tmpdir=$(mktemp -d)
 
 trap "rm -rf $tmpdir" EXIT
 
-self="$0"
-basename_of_self="$(basename "$self")"
-
-# I want to distinguish "me" and "./me" of $0.
-# The difference between this method and `dirname` is:
-# dirname converts "" to "./"
-# but this method doesn't.
-# For example if $0 == "me"
-# `dirname $0` == "./"
-# $dirname_of_self == ""
-dirname_of_self="${self%$basename_of_self}"
-
-if [ -n "$dirname_of_self" ];then
-    # when this executable executed as a command in $PATH.
-
-    # get full path of $0
-    self="$(command -v "$0")"
-fi
-
+# if $0 is an ablosute path or a relative path,
+# $self = $0
+# if $0 is a command in $PATH,
+# $self is absolute path of $0
+self="$(command -v "$0")"
 
 tail -n +$_archive_start_line < "$self" | gunzip | tar -x -C $tmpdir
 
