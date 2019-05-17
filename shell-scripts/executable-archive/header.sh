8c0c7e53f2146c9cfd7e7be84401c9e9f0e74136
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2019-05-18 02:28:58 +0900

    [executable-archive] Handle the case of the archive executed as a command in $PATH

--- shell-scripts/executable-archive/header.sh
+++ shell-scripts/executable-archive/header.sh
@@ -5,7 +5,27 @@ tmpdir=$(mktemp -d)
 
 trap "rm -rf $tmpdir" EXIT
 
-tail -n +$_archive_start_line < $0 | gunzip | tar -x -C $tmpdir
+self="$0"
+basename_of_self="$(basename "$self")"
+
+# I want to distinguish "me" and "./me" of $0.
+# The difference between this method and `dirname` is:
+# dirname converts "" to "./"
+# but this method doesn't.
+# For example if $0 == "me"
+# `dirname $0` == "./"
+# $dirname_of_self == ""
+dirname_of_self="${self%$basename_of_self}"
+
+if [ -n "$dirname_of_self" ];then
+    # when this executable executed as a command in $PATH.
+
+    # get full path of $0
+    self="$(command -v "$0")"
+fi
+
+
+tail -n +$_archive_start_line < "$self" | gunzip | tar -x -C $tmpdir
 
 $tmpdir/"$_script" "$@"
 exit
