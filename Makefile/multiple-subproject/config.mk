3b98f9c4bf282fe1fb28c205bbd9fb8de804567f
Yutaro Orikasa <yutaro.orikasa.625@gmail.com>
2022-11-21 08:30:29 +0900

    [Makefile/multiple-subproject] Fix config.mk
    Makefiles were copied from multiple-executables and modified.
    _build is no longer created.

--- Makefile/multiple-subproject/config.mk
+++ Makefile/multiple-subproject/config.mk
@@ -2,7 +2,7 @@
 
 # Rewrite here for your project
 LIBRARY_SOURCE_DIRS ?= projects/liba projects/libb projects/libc projects/libreqa
-EXECUTABLE_SOURCE_DIRS ?= projects/a projects/abc projects/x
+EXECUTABLE_SOURCE_DIRS ?= projects/a projects/abc projects/x projects/req-libreqa
 INCLUDE_PATHS ?= # projects/include
 # single file libraries/executables
 # LIBRARY_SOURCE_FILES ?= test/src/libtest.cpp
